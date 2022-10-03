fd19c0129965259411c72f4b23c7fff71b5f024f
Assane DIALLO <assane@pando2.com>
2022-10-03 18:04:08 +0200

    Add articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,9 @@ class ArticlesController < ApplicationController
       #byebug #this is a debugger tool
       @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
   
 end
